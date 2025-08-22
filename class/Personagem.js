95e8c2be7d456afe71653221498aa1f601ca9380
✧ Sophia Cordeiro✧ <emilisophiassss@gmail.com>
2025-08-22 07:31:42 -0300

    Update Personagem.js

--- class/Personagem.js
+++ class/Personagem.js
@@ -1,33 +1,31 @@
-class Personagem {
+class Personagem{
     #vida
-    constructor(nome,classe,vida){
+    constructor(nome, classe, vida){
         this.nome = nome
-        this.classe =classe
-        this.#vida =vida
+        this.classe = classe
+        this.#vida = vida
     }
 
     atacar(){
-        console.log(`${this.nome} (${this.classe}) fez um ataque!`)
+        console.log(`${this.nome} (${this.classe}) realiza um grande ataque!`)
     }
-
-    mostrarInformacoes(){
-        console.log(`nome:${this.nome}`)
-        console.log(`classe:${this.classe}`)
-        console.log(`vida:${this.#vida}`)
+    mostrarInfo(){
+        console.log(`nome: ${this.nome}`);
+        console.log(`classe: ${this.classe}`);
+        console.log(`pontos de vida: ${this.#vida}`)
     }
 
     getVida(){
-        return this.#vida
+        return this.#vida;
     }
-
     setVida(novaVida){
-        if(novaVida>=0){
-            this.#vida = novaVida
-        }else{
-            console.log(`vida não pode ser negativa`)
+        if (novaVida >= 0) {
+            this.#vida = novaVida;
+        } else{
+            console.log("vida nao pode ser negativa")
         }
-    }
 
+    }
 }
 
 module.exports = Personagem
